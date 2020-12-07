2c329fc49bf52fc1cb6689e84b796cd5a518cc49
anurudhp <anurudhp97@gmail.com>
2020-12-07 20:47:51 +0530

    add day 5

--- Helpers.hs
+++ Helpers.hs
@@ -1,17 +1,22 @@
 module Helpers where
 
+-- count number of elements satisfying the predicate
 countPred :: Eq a => (a -> Bool) -> [a] -> Int
 countPred p = length . filter p
 
+-- count number of elements equal to a value
 count :: Eq a => a -> [a] -> Int
 count x = countPred (== x)
 
+-- split into chunks of fixed size
 chunksOf :: Int -> [a] -> [[a]]
 chunksOf _ [] = []
 chunksOf n xs =
   let (hs, ts) = splitAt n xs
    in hs : chunksOf n ts
 
+-- split on predicate
+-- all values satisfying the predicate are dropped
 splitOn :: (a -> Bool) -> [a] -> [[a]]
 splitOn _ [] = [[]]
 splitOn p (x:xs)
@@ -19,3 +24,10 @@ splitOn p (x:xs)
   | otherwise = (x : g) : gs
   where
     (g:gs) = splitOn p xs
+
+-- Convert a binary string to a number
+strToBin :: String -> Int
+strToBin = foldl (\acc b -> 2 * acc + b) 0 . map bit
+  where
+    bit '0' = 0
+    bit '1' = 1
---
+++ day5.hs
@@ -0,0 +1,27 @@
+import Control.Arrow ((>>>))
+import Data.List ((\\), sort)
+import Helpers (strToBin)
+
+main :: IO ()
+main =
+  interact $
+  lines >>> ([highestSeat, findMySeat] <*>) . pure >>> map show >>> unlines
+
+-- part 1
+highestSeat :: [String] -> Int
+highestSeat = maximum . map seatToID
+
+-- part 2
+findMySeat :: [String] -> Int
+findMySeat seats = head $ range \\ occ
+  where
+    occ = sort . map seatToID $ seats
+    range = [minimum occ .. maximum occ]
+
+seatToID :: String -> Int
+seatToID = strToBin . map conv
+  where
+    conv 'F' = '0'
+    conv 'B' = '1'
+    conv 'L' = '0'
+    conv 'R' = '1'
