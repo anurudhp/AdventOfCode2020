ec46cf87f029a3055a921761cf98ad8510169d0d
anurudhp <anurudhp97@gmail.com>
2020-12-25 04:03:01 +0530

    update day11 part 2

--- src/day11.hs
+++ src/day11.hs
@@ -11,30 +11,39 @@ main =
   interact $
   lines >>>
   mkGrid >>>
-  (stepTillStable <$> [stepCell1, stepCell2] <*>) . pure >>>
+  (stepTillStable . stepCell <$> [neighbours1, neighbours2] <*>) . pure >>>
   map (count Occupied) >>> show
 
--- part 1
 type StepCellFn = Grid -> Loc -> Cell -> Cell
 
-neighbours :: Loc -> Grid -> [Cell]
-neighbours (x, y) g =
-  catMaybes $
-  M.lookup <$>
-  [(x + dx, y + dy) | dx <- [-1, 0, 1], dy <- [-1, 0, 1], dx /= 0 || dy /= 0] <*>
-  pure g
+type NeighbourCells = Loc -> Grid -> [Cell]
 
-stepCell1 :: StepCellFn
-stepCell1 g l cur
+stepCell :: NeighbourCells -> StepCellFn
+stepCell neighbours g l cur
   | cur == Empty && occ == 0 = Occupied
   | cur == Occupied && occ >= 4 = Empty
   | otherwise = cur
   where
     occ = count Occupied (neighbours l g)
 
+-- part 1
+neighbours1 :: Loc -> Grid -> [Cell]
+neighbours1 (x, y) g =
+  catMaybes $
+  M.lookup <$>
+  [(x + dx, y + dy) | dx <- [-1, 0, 1], dy <- [-1, 0, 1], dx /= 0 || dy /= 0] <*>
+  pure g
+
 -- part 2
-stepCell2 :: StepCellFn
-stepCell2 g l cur = undefined
+neighbours2 :: Loc -> Grid -> [Cell]
+neighbours2 (x, y) g =
+  catMaybes
+    [ head . (++ [Nothing]) . dropWhile (== Just Floor) $
+    [M.lookup (x + dx * r, y + dy * r) g | r <- [1 .. 100]]
+    | dx <- [-1, 0, 1]
+    , dy <- [-1, 0, 1]
+    , dx /= 0 || dy /= 0
+    ]
 
 -- helpers
 data Cell
@@ -51,6 +60,7 @@ mkCell :: Char -> Cell
 mkCell '.' = Floor
 mkCell 'L' = Empty
 mkCell '#' = Occupied
+mkCell _ = undefined
 
 mkGrid :: [String] -> Grid
 mkGrid ip =
